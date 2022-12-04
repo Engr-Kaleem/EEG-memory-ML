98f0fc267b1c79123dce14dc87ca169af1284ea7
Kaleem Ullah <49053078+Engr-Kaleem@users.noreply.github.com>
2022-12-05 03:50:52 +0500

    accuracy plots and confusion matrices

---
+++ SMOTE_randomforest.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+from statistics import mean
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
+import mne
+from MLEngine import FilterBank
+from FBCSP import FBCSP
+from MLEngine import MLEngine
+import CSP
+
+epochs = mne.read_epochs('data/clean_s10_erp_epochs.fif', preload=True)
+print(epochs['EV_ENC'].shape,epochs[EV_NO_ENC].hsape)
+eeg_data = epochs.get_data()
+fbank = FilterBank(fs=100.0)
+fbank_coeff = fbank.get_filter_coeff()
+filtered_data = fbank.filter_data(eeg_data)
+labels = epochs.events[:, -1]
+labels[labels==2]=0
+print(labels)
+""" #Use SMOTE to oversample the minority class
+oversample = SMOTE()
+over_X, over_y = oversample.fit_resample(X, y)
+over_X_train, over_X_test, over_y_train, over_y_test = train_test_split(over_X, over_y, test_size=0.1, stratify=over_y)
+#Build SMOTE SRF model
+SMOTE_SRF = RandomForestClassifier(n_estimators=150, random_state=0)
+#Create Stratified K-fold cross validation
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+scoring = ('f1', 'recall', 'precision')
+#Evaluate SMOTE SRF model
+scores = cross_validate(SMOTE_SRF, over_X, over_y, scoring=scoring, cv=cv)
+#Get average evaluation metrics
+print('Mean f1: %.3f' % mean(scores['test_f1']))
+print('Mean recall: %.3f' % mean(scores['test_recall']))
+print('Mean precision: %.3f' % mean(scores['test_precision']))
+
+#Randomly spilt dataset to test and train set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, stratify=y)
+#Train SMOTE SRF
+SMOTE_SRF.fit(over_X_train, over_y_train)
+#SMOTE SRF prediction result
+y_pred = SMOTE_SRF.predict(X_test)
+#Create confusion matrix
+fig = plot_confusion_matrix(SMOTE_SRF, X_test, y_test, display_labels=['Will Not Buy', 'Will Buy'], cmap='Greens')
+plt.title('SMOTE + Standard Random Forest Confusion Matrix')
+plt.show() """
---
+++ metrics_plot.py
@@ -0,0 +1,39 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+def  plot_metrics(All_metrics,clf):
+    
+    plt.figure()
+    plt.plot(All_metrics[0,:],All_metrics[1,:],marker='P' ,label = "train_acc")
+    plt.plot(All_metrics[0,:],All_metrics[2,:],marker='P' ,label = "test_acc")
+    plt.plot(All_metrics[0,:],All_metrics[3,:],marker='P' , label = "F1")
+    plt.plot(All_metrics[0,:],All_metrics[4,:],marker='P' , label = "Recall")
+    plt.plot(All_metrics[0,:],All_metrics[5,:],marker='P' , label = "Precision")
+  
+    plt.xlabel('subjects')
+    plt.ylabel('Accuracy')
+    plt.title(f'Accuracey with clssifier:{clf}')
+      
+
+    # Put a legend below current axis
+    plt.legend(loc='upper right',fontsize = 'xx-small')
+    #plt.show()
+    plt.savefig(f'Accuracey/clssifier{clf}.png')
+
+
+   
+
+
+def conf_matrix(y_test, pred_test,sub,i,k):    
+    
+    # Creating a confusion matrix
+    con_mat = confusion_matrix(y_test, pred_test)
+    con_mat = pd.DataFrame(con_mat, range(2), range(2))
+   
+    #Ploting the confusion matrix
+    plt.figure(figsize=(6,6))
+    sns.set(font_scale=1.5) 
+    g=sns.heatmap(con_mat, annot=True, annot_kws={"size": 16}, fmt='g', cmap='Blues', cbar=False)
+    g.set_title(f'subj{sub}fold{i}nthtime{k}')
+    plt.savefig(f'confmatSVR/subj{sub}fold{i}nthtime{k}')
+    plt.close()
+    
